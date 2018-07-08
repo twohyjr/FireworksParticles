df30a2368dfd5463b26585622c7031076a2417b7
Rick Twohy <twohyjr@gmail.com>
2018-07-08 16:26:10 -0500

    Initial Commit

---
+++ FireworksParticles/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FireworksParticles
+//
+//  Created by Twohy on 7/8/18.
+//  Copyright © 2018 TwohyTutorials. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
